52e5aad4bc901f29d2d1acae68b9174231cc6c8a
Barp24 <barpesahov@gmail.com>
2022-06-18 19:00:04 +0300

    New project

---
+++ iHotel/iHotel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iHotel
+//
+//  Created by Bar P on 18/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
