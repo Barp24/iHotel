de24162ed811d9bef26bbd1793a0e87b11b136d9
Barp24 <barpesahov@gmail.com>
2022-08-23 19:43:54 +0300

    Fix warnings

--- iHotel/iHotel/RegisterViewController.swift
+++ iHotel/iHotel/RegisterViewController.swift
@@ -21,7 +21,7 @@ class RegisterViewController: UIViewController, UIImagePickerControllerDelegate 
             loading.startAnimating()
             Auth.auth().createUser(withEmail: emailText.text!, password: passwordText.text!) { authResult, error in
                 if error != nil {
-                    print(error)
+                    print(error ?? "error while registering")
                     self.loading.stopAnimating()
 //                    if [AuthErrorCode.networkError, AuthErrorCode.invalidEmail, AuthErrorCode.emailAlreadyInUse].contains(AuthErrorCode(rawValue: error._code)){
 //                        self.displayAlert(message: error.localizedDescription)
